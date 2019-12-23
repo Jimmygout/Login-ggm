f40653ee4906bf044978287d5e93755000d1202a
Jimmy Gout <j.gout@conectis.net>
2019-12-23 15:48:02 +0100

    Ajout script pour transformer tout les mdp base64 en argon2i de la table ggmcontact

---
+++ password_update.php
@@ -0,0 +1,40 @@
+<?php
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=user_exemple;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT prenom,nom,pk_ggm_contact, pass FROM ggm_contact');
+
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+    ?>
+
+    <?php
+    // récuperation de l'id
+    $id = $donnees['pk_ggm_contact'];
+    // 60 caractères
+    $options = [
+        'cost' => 12,
+    ];
+    // Si le pass est toujours en base64 alor on decode puis on encrypte en BCRYPT
+    if(strlen($donnees['pass']) < 50)
+    {
+        $new_password = password_hash(base64_decode($donnees['pass']), PASSWORD_ARGON2ID, $options);
+        $update = $bdd->query("UPDATE ggm_contact SET pass = '$new_password' WHERE pk_ggm_contact = '$id'");
+        echo $donnees['prenom'].' '.$donnees['nom'].' pass =' . $new_password; ?><br/>
+        <?php
+    }
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
